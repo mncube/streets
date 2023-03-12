32aa2fdfbe204db36600ce9b0d7a80c3e1c5d106
Mackson Ncube <macksonncube@gmail.com>
2023-03-12 22:19:15 +0000

    Write mpaths function

--- R/mpaths.R
+++ R/mpaths.R
@@ -1,3 +1,16 @@
+#' Manhattan Paths
+#'
+#' The mpaths function computes the number of paths with equal Manhattan Distance
+#' starting at the origin (0,0) and ending at (i, j).
+#'
+#' @param i number of rows in the 2D lattice
+#' @param j number of columns in the 2D lattice
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' mpaths(i = 2, j = 2)
 mpaths <- function(i,j){
 
   if (i < 0 | j < 0){
