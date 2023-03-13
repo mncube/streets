6dfcfd25ac0a14c1865c3af0ae2541d86237bc8d
Mackson Ncube <macksonncube@gmail.com>
2023-03-13 15:20:58 +0000

    document dpaths

--- R/dpaths.R
+++ R/dpaths.R
@@ -1,3 +1,20 @@
+#' Detour Paths
+#'
+#' The dpaths function computes the number of paths with equal Manhattan Distance
+#' starting at the origin (0,0) NOT passing through the detour coordinate (di, dj) and
+#' ending at the destination (i, j).
+#'
+#' @param destination A vector defining the destination coordinates in terms of
+#' a horizontal coordinate i and a vertical coordinate j
+#'
+#' @param detour A vector defining the detour coordinates in terms of a horizontal
+#' coordinate di and a vertical coordinate dj
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' dpaths(destination = c(4,4), detour = c(2,2))
 dpaths <- function(destination = c(i,j), detour = c(di, dj)){
 
   # Get values
