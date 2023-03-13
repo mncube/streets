c00bfad8905e32aca3b8d7a0048e92b2b1b4b514
Mackson Ncube <macksonncube@gmail.com>
2023-03-13 02:02:17 +0000

    tpaths tests

---
+++ tests/testthat/test-tpaths.R
@@ -0,0 +1,10 @@
+test_that("tpaths gives same answer as mpaths when ti = i and tj = j", {
+  expect_equal(tpaths(destination = c(4,4), target = c(4,4)),
+               mpaths(i = 4, j = 4))
+})
+
+
+test_that("tpaths throws an error when i < ti or j < tj", {
+  expect_error(tpaths(destination = c(3,3), target = c(4,2)))
+  expect_error(tpaths(destination = c(3,3), target = c(2,4)))
+})
