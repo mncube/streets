15943dce81a5e4730b64f518790e7dbdf1a3ac1a
Mackson Ncube <macksonncube@gmail.com>
2023-03-15 18:17:41 +0000

    start on target_probability_funs

--- R/target_probability_funs.R
+++ R/target_probability_funs.R
@@ -14,8 +14,8 @@
 #' @export
 #'
 #' @examples
-#' pftarg(destination = c(5,5), target = c(1,1))
-pftarg <- function(destination = c(i, j),
+#' ptpaths(destination = c(5,5), target = c(1,1))
+ptpaths <- function(destination = c(i, j),
                    target = c(ti, tj)){
   #Get values
   i <- destination[[1]]
@@ -26,61 +26,3 @@ pftarg <- function(destination = c(i, j),
   tpaths(destination = destination,
          target = target)/mpaths(destination = destination)
 }
-
-# ptarg <- function(destination = c(i, j),
-#                   target = c(ti, tj)){
-#
-#   #Get values
-#   i <- destination[[1]]
-#   j <- destination[[2]]
-#   ti <- target[[1]]
-#   tj <- target[[2]]
-#
-#   length(0:ti)*length(0:tj)
-#
-#   sum(my_pmf(1:x))
-#   sum(pftarg)
-#   integrate(pftarg, -Inf, destination, target)$value
-#
-# }
-
-
-# ptarg <- function(destination = c(i, j), target = c(ti, tj), x){
-#
-#   #Might need to evaluate at all points and grid and get proportion of points
-#   #with less paths than at the target point.
-#
-#   #Get values
-#   i <- destination[[1]]
-#   j <- destination[[2]]
-#   ti <- target[[1]]
-#   tj <- target[[2]]
-#
-#   #Calculate cumulative probability
-#   cum_prob <- 0
-#   for (k in 0:x){
-#     cum_prob <- cum_prob + pftarg(destination, target = c(ti + k, tj + x - k))
-#   }
-#
-#   return(cum_prob)
-# }
-#
-#
-# # ptarg <- function(destination = c(i, j), target = c(ti, tj)){
-# #
-# #   #Get values
-# #   i <- destination[[1]]
-# #   j <- destination[[2]]
-# #   ti <- target[[1]]
-# #   tj <- target[[2]]
-# #
-# #   #Calculate cumulative probability
-# #   cum_prob <- 0
-# #   for (iti in 0:ti){
-# #     for(itj in 0:tj){
-# #       cum_prob <- cum_prob + pftarg(destination, target = c(iti, itj))
-# #     }
-# #   }
-# #
-# #   return(cum_prob)
-# # }
--- tests/testthat/test-target_probability_funs.R
+++ tests/testthat/test-target_probability_funs.R
@@ -1,7 +1,31 @@
-test_that("pftarg gives 1 when the target location equals the destination and
+test_that("ptpaths gives 1 when the target location equals the destination and
           when the target location is at the origin", {
 
-            expect_equal(pftarg(destination = c(5,5), target = c(5,5)), 1)
-            expect_equal(pftarg(destination = c(5,5), target = c(0,0)), 1)
+            expect_equal(ptpaths(destination = c(5,5), target = c(5,5)), 1)
+            expect_equal(ptpaths(destination = c(5,5), target = c(0,0)), 1)
 
+})
+
+
+test_that("ptpaths returns correct values and behaves as expected", {
+
+  # Test 1: Test with target at origin (0, 0)
+  expect_equal(ptpaths(destination = c(4, 4), target = c(0, 0)), 1)
+
+  # Test 2: Test with target at destination
+  expect_equal(ptpaths(destination = c(4, 4), target = c(4, 4)), 1)
+
+  # Test 3: Test with a symmetric case
+  expect_equal(ptpaths(destination = c(10,10), target = c(4,8)),
+               ptpaths(destination = c(10,10), target = c(8,4)))
+
+  # Test 4: Test with another symmetric case
+  expect_equal(ptpaths(destination = c(20, 20), target = c(19,19)),
+               ptpaths(destination = c(20, 20), target = c(1,1)))
+
+  # Test 5: Test with q = 0
+  expect_equal(ptpaths(destination = c(0, 0), target = c(0, 0)), 1)
+
+  # Test 6: Test with invalid target (greater than destination)
+  expect_error(ptpaths(destination = c(2, 2), target = c(3, 3)), "Target coordinate must be on grid")
 })
