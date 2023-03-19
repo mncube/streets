0134915fa86a6ebefedfbb0ff56f54b5ac8ccf19
Mackson Ncube <macksonncube@gmail.com>
2023-03-19 01:32:48 +0000

    write bpaths function

---
+++ R/bpaths.R
@@ -0,0 +1,56 @@
+#' Branch Paths
+#'
+#' The bpaths function computes the number of paths starting at the origin (0,0)
+#' passing through the branch coordinate (bi, bj) and ending at the destination
+#' (i, j).  At non-branch coordinates, the path is only allowed to step upwards
+#' and rightwards; however, at the branch coordinate, the path is allowed to step
+#' in any direction except the direction in which the path entered the branch
+#' coordinate.  This is similar to a no-U turn intersection where a car can go
+#' straight, turn left, or turn right.
+#'
+#' @param destination A vector defining the destination coordinates in terms of
+#' a horizontal coordinate i and a vertical coordinate j
+#' @param branch  A vector defining the branch coordinates in terms of a horizontal
+#' coordinate bi and a vertical coordinate bj
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+#' bpaths(c(3,3), c(2,2))
+bpaths <- function(destination = c(i,j), branch = c(bi, bj)){
+
+  #Get values
+  i <- destination[[1]]
+  j <- destination[[2]]
+  bi <- branch[[1]]
+  bj <- branch[[2]]
+
+  if (bi > i | bj > j){
+    rlang::abort(message = "Branch coordinate must be on grid.  Make sure i >= bi and
+                 j >= bj.")
+  }
+
+  if (bi < 0 | bj < 0){
+    rlang::abort(message = "bi and bj must be non-negative.")
+  }
+
+  if (bi != floor(bi) | bj != floor(bj)){
+    rlang::abort(message = "bi and bj must be non-negative integers")
+  }
+
+  #Compute branch paths
+  if (bi == 0 || bj == 0){
+    tpaths(destination = destination, target = branch)
+  } else if (bi == i){
+    mpaths(c(bi, bj-1))*(mpaths(c(i-bi, j-bj)) + mpaths(c(i -(bi-1), j-(bj+1)))) +
+      mpaths(c(bi-1,bj))*mpaths(c(i-bi,j-bj))
+  } else if (bj==j) {
+    mpaths(c(bi, bj-1))*mpaths(c(i-bi, j-bj)) +
+      mpaths(c(bi-1,bj))*(mpaths(c(i-bi,j-bj)) + mpaths(c(i-(bi+1), j-(bj-1))))
+  }else {
+    mpaths(c(bi, bj-1))*(mpaths(c(i-bi, j-bj)) + mpaths(c(i -(bi-1), j-(bj+1)))) +
+      mpaths(c(bi-1,bj))*(mpaths(c(i-bi,j-bj)) + mpaths(c(i-(bi+1), j-(bj-1))))
+  }
+
+}
---
+++ tests/testthat/test-bpaths.R
@@ -0,0 +1,21 @@
+test_that("bpaths works as expected", {
+
+  #Test 1: Check bpaths against manual computation from notebooks
+  expect_equal(dpaths(c(3,3), c(2,2)) + bpaths(c(3,3), c(2,2)), 26)
+
+  #Test 2: Check bpaths against manual computation from notebooks
+  expect_equal(dpaths(c(3,3), c(2, 1)) + bpaths(c(3,3), c(2,1)), 25)
+
+  #Test 3: Check bpaths against manual computation from notebooks
+  #This also serves as an edge case test with bj == 0
+  expect_equal(dpaths(c(3,3), c(2, 0)) + bpaths(c(3,3), c(2,0)), 20)
+
+  #Test 4: Check that bpaths == tpaths when bi == 0
+  expect_equal(bpaths(c(5,5), c(0, 3)), tpaths(c(5,5), c(0, 3)))
+
+  #Test 5:The bi == i edge case does not throw an error
+  expect_no_error(bpaths(c(5,5), c(5,4)))
+
+  #Test 6:The bj == j edge case does not throw an error
+  expect_no_error(bpaths(c(5,5), c(4,5)))
+})
