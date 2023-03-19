30f2ed212e1b70c983c363d6fba2326afb3de5a7
Mackson Ncube <macksonncube@gmail.com>
2023-03-19 01:59:40 +0000

    write pbpaths function

--- R/branch_probability_funs.R
+++ R/branch_probability_funs.R
@@ -36,3 +36,41 @@ dbpaths <- function(destination = c(i, j),
   bpaths(destination = destination,
          branch = branch)/all_b_paths
 }
+
+
+#' Branch Paths Cumulative Distribution Function
+#'
+#' A cumulative distribution function for the dbpaths probability mass function
+#'
+#' @param destination A vector defining the destination coordinates in terms of
+#' a horizontal coordinate i and a vertical coordinate j.
+#' @param branch A vector defining the branch coordinates in terms of a
+#' horizontal coordinate bi and a vertical coordinate bj.
+#'
+#' @return A real number in the closed interval 0 to 1
+#' @export
+#'
+#' @examples
+#' pbpaths(destination = c(5,5), branch = c(1,1))
+pbpaths <- function(destination = c(i, j), branch = c(bi, bj)) {
+  # Get values
+  i <- destination[[1]]
+  j <- destination[[2]]
+  bi <- branch[[1]]
+  bj <- branch[[2]]
+
+  # Initialize the cumulative probability
+  cumulative_probability <- 0
+
+  # Iterate through all possible branch coordinates up to and including (bi, bj)
+  for (x in 0:bi) {
+    for (y in 0:bj) {
+      # Add the probability of each branch coordinate to the cumulative probability
+      cumulative_probability <- cumulative_probability +
+        dbpaths(destination = destination, branch = c(x, y))
+    }
+  }
+
+  # Return the cumulative probability
+  return(cumulative_probability)
+}
--- tests/testthat/test-branch_probability_funs.R
+++ tests/testthat/test-branch_probability_funs.R
@@ -19,3 +19,20 @@ test_that("dbpaths is working as expected", {
   }
   expect_equal(cumpaths, 1)
 })
+
+
+test_that("pbpaths returns correct values and behaves as expected", {
+  # Test 1: When the target is the origin (0, 0)
+  expect_equal(pbpaths(c(3, 3), c(0, 0)), dbpaths(c(3, 3), c(0, 0)))
+
+  # Test 2: When the target is the destination (i, j)
+  expect_equal(pbpaths(c(3, 3), c(3, 3)), 1)
+
+  # Test 3: Cumulative probability function is symmetric
+  expect_equal(pbpaths(c(3, 3), c(2, 1)), pbpaths(c(3, 3), c(1, 2)))
+  expect_equal(pbpaths(c(10, 10), c(3, 8)), pbpaths(c(10, 10), c(8, 3)))
+
+  # Test 4: When the target coordinate is off the grid
+  expect_error(pbpaths(c(3, 3), c(4, 3)), "Branch coordinate must be on grid")
+  expect_error(pbpaths(c(3, 3), c(3, 4)), "Branch coordinate must be on grid")
+})
